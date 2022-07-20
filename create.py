134984eedd05976228cde17d2b386135bd659d0d
MaksimAnatolievich <106883916+MaksimAnatolievich@users.noreply.github.com>
2022-07-20 15:01:53 +0300

    change delete functions

--- create.py
+++ create.py
@@ -1,11 +1,14 @@
-from JSON.validation import validate_username
+from JSON.validation import validate_username,validate_age,validate_password
 from services import create_user, get_users
+
 while True:
     try :
         username = input("Введите имя пользователя\n")
         password = input("Введите пароль\n")
         age = int(input("Введите возраст\n"))
         validate_username(username)
+        validate_password(password)
+        validate_age(age)
     except Exception as error:
         print(f'Данные некорректные: {error}')
     else:
